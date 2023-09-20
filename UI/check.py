d42d6c082858992d46e12179c52724b3a98b8654
Lykos2 <pasunti.prasanjith@pilani.bits-pilani.ac.in>
2023-09-20 08:33:06 +0000

    found error retriever

--- UI/check.py
+++ UI/check.py
@@ -86,8 +86,8 @@ with st.sidebar:
 
 if result:
     if MODEL!="":
-        #MODEL_BASENAME="llama-2-7b-chat.ggmlv3.q4_0.bin"
-        MODEL_BASENAME = "nous-hermes-13b-GPTQ-4bit-128g.no-act.order"
+        MODEL_BASENAME="llama-2-7b-chat.ggmlv3.q4_0.bin"
+        #MODEL_BASENAME = "nous-hermes-13b-GPTQ-4bit-128g.no-act.order"
         llm = load_model(device_type, model_id=MODEL, model_basename=MODEL_BASENAME)
         st.session_state['llm']=llm
    
@@ -113,6 +113,7 @@ if result:
     faiss = FAISS.from_texts(chunks, st.session_state['emb'])
     if 'faiss' not in st.session_state:
         st.session_state['faiss']=faiss
+    retriever=st.session_state['faiss'].as_retriever()
     
     template = """Use the following pieces of context to answer the question at the end. If you don't know the answer,\
     just say that you don't know, don't try to make up an answer.
@@ -133,7 +134,7 @@ if result:
         qa = RetrievalQA.from_chain_type(
         llm=st.session_state['llm'],
         chain_type="stuff",
-        retriever =  st.session_state['faiss'],
+        retriever = retriever,
         return_source_documents=True,
         chain_type_kwargs={"prompt": prompt, "memory": memory},
         )
